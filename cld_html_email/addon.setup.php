9086e7e3e913ed1c11786efc928499b17f57c7a1
Simon Balz <simon@balz.me>
2017-04-18 20:21:23 +0200

    xAdded addon.setup.php for EE3 compatibility

---
+++ cld_html_email/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'author'      => 'James Smith',
+  'author_url'  => 'http://www.jamessmith.co.uk',
+  'name'        => 'CLD HTML Email',
+  'description' => 'Sets all outgoing EE email to use the format specified in the control panel config settings',
+  'version'     => '1.0',
+  'namespace'   => 'CLD\HTML_Email'
+);
